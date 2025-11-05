b870999d1335f2d96430ce716b04c97a97784d7e
Yuriy Li <0yuriyli@gmail.com>
2025-11-06 00:59:56 +0300

    feat: add solution carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -17,11 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+	if (typeof sampleActivity !== "string") {
+		return false;
+	}
+	const numberSample = Number(sampleActivity);
+	if (typeof numberSample !== "number" || Number.isNaN(numberSample)) {
+		return false;
+	}
+	if (numberSample <= 0 || numberSample > MODERN_ACTIVITY) {
+		return false;
+	}
+	return Math.ceil(
+		-1 * (HALF_LIFE_PERIOD / Math.LN2) * Math.log(numberSample / MODERN_ACTIVITY)
+	);
 }
 
 module.exports = {
-  dateSample
+	dateSample,
 };
