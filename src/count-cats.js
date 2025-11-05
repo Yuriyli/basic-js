182270fa792f4fdec4b5ed7d5c9ef55a66daba16
Yuriy Li <0yuriyli@gmail.com>
2025-11-06 01:59:38 +0300

    feat: add solution count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
@@ -14,11 +14,18 @@ const { NotImplementedError } = require('../lib');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+	let result = 0;
+	for (const cArray of matrix) {
+		for (const c of cArray) {
+			if (c === "^^") {
+				++result;
+			}
+		}
+	}
+	return result;
 }
 
 module.exports = {
-  countCats
+	countCats,
 };
