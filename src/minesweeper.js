85e52bfb20d4e294c8b12f34b2fe9efcea5b6665
Yuriy Li <0yuriyli@gmail.com>
2025-11-07 02:31:03 +0300

    feat: add solution minesweeper

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,47 @@ const { NotImplementedError } = require('../lib');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+	let result = [];
+
+	const maxY = matrix.length - 1;
+	const maxX = matrix[0].length - 1;
+  
+	for (let y = 0; y < matrix.length; y++) {
+		const subArray = [];
+		for (let x = 0; x < matrix[y].length; x++) {
+			subArray.push(scanAround(x, y));
+		}
+		result.push(subArray);
+	}
+
+	return result;
+
+	function scanAround(x, y) {
+		let result = 0;
+		for (let offsetY = -1; offsetY < 2; offsetY++) {
+			for (let offsetX = -1; offsetX < 2; offsetX++) {
+				if (offsetY === 0 && offsetX === 0) {
+					continue;
+				}
+
+				const currentY = y + offsetY;
+				if (currentY < 0 || currentY > maxY) {
+					continue;
+				}
+
+				const currentX = x + offsetX;
+				if (currentX < 0 || currentX > maxX) {
+					continue;
+				}
+        
+				result += matrix[currentY][currentX];
+			}
+		}
+		return result;
+	}
 }
 
 module.exports = {
-  minesweeper
+	minesweeper,
 };
