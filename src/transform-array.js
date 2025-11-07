12ce4930a013a66b99dbd51fd18d4e85cdf0187c
Yuriy Li <0yuriyli@gmail.com>
2025-11-07 06:56:22 +0300

    feat: add solution transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Create transformed array based on the control sequences that original
@@ -13,11 +13,100 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+	const result = [];
+
+	if (!Array.isArray(arr)) {
+		throw new Error("'arr' parameter must be an instance of the Array!");
+	}
+
+	let flagNextDeleted = false;
+	let flagNextDoubled = false;
+	let flagPrevDeleted = false;
+	let flagPrevDoubled = false;
+
+	for (let i = 0; i < arr.length; ) {
+		const element = arr[i];
+
+		if (typeof element === "number") {
+			result.push(element);
+			flagNextDeleted = false;
+			flagNextDoubled = false;
+			flagPrevDeleted = false;
+			flagPrevDoubled = false;
+			++i;
+			continue;
+		}
+
+		switch (element) {
+			case "--discard-next":
+				if (i === arr.length - 1) {
+					++i;
+					break;
+				}
+
+				flagNextDeleted = true;
+				flagNextDoubled = false;
+				flagPrevDeleted = false;
+				flagPrevDoubled = false;
+				i += 2;
+				break;
+
+			case "--discard-prev":
+				if (i === 0) {
+					++i;
+					break;
+				}
+
+				if (!flagNextDeleted) {
+					result.splice(-1, 1);
+				}
+				flagNextDeleted = false;
+				flagNextDoubled = false;
+				flagPrevDeleted = true;
+				flagPrevDoubled = false;
+
+				++i;
+				break;
+
+			case "--double-next":
+				if (i === arr.length - 1) {
+					++i;
+					break;
+				}
+
+				result.push(arr[i + 1]);
+				result.push(arr[i + 1]);
+
+				flagNextDeleted = false;
+				flagNextDoubled = true;
+				flagPrevDeleted = false;
+				flagPrevDoubled = false;
+
+				i += 2;
+				break;
+
+			case "--double-prev":
+				if (i === 0) {
+					++i;
+					break;
+				}
+				if (!flagNextDeleted) {
+					result.push(result[result.length - 1]);
+				}
+				flagNextDeleted = false;
+				flagNextDoubled = false;
+				flagPrevDeleted = false;
+				flagPrevDoubled = true;
+
+				++i;
+				break;
+		}
+	}
+
+	return result;
 }
 
 module.exports = {
-  transform
+	transform,
 };
