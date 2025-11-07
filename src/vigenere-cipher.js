2dd2412bfffa30cb2c8356f958b3f9a5f77a7838
Yuriy Li <0yuriyli@gmail.com>
2025-11-07 20:48:16 +0300

    feat: add solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
@@ -20,19 +20,101 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
-  }
+	#direct;
+	constructor(direct = true) {
+		this.#direct = direct;
+	}
+	encrypt(msg, key) {
 
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
-  }
+		if (typeof msg !== "string" || typeof key !== "string") {
+			throw new Error("Incorrect arguments!");
+		}
+
+		let result = "";
+		let keyArr = [];
+		let keyIndex = -1;
+
+		for (let i = 0; i < key.length; i++) {
+			keyArr.push(key.toUpperCase().codePointAt(i) - 65);
+		}
+		for (const char of msg.toUpperCase()) {
+			if (/[A-Z]/.test(char)) {
+				result += shiftChar(char);
+			} else {
+				result += char;
+			}
+		}
+
+		if (!this.#direct) {
+			result = Array.from(result).reverse().join("");
+		}
+
+		return result;
+
+		function shiftChar(char) {
+			let result = char.codePointAt(0);
+			result = result + getCurrentKey();
+			if (result > 90) {
+				result = result - 26;
+			}
+			return String.fromCharCode(result);
+		}
+
+		function getCurrentKey() {
+			++keyIndex;
+			if (keyIndex >= key.length) {
+				keyIndex = 0;
+			}
+			return keyArr[keyIndex];
+		}
+	}
+
+	decrypt(msg, key) {
+
+		if (typeof msg !== "string" || typeof key !== "string") {
+			throw new Error("Incorrect arguments!");
+		}
+
+		let result = "";
+		let keyArr = [];
+		let keyIndex = -1;
+
+		for (let i = 0; i < key.length; i++) {
+			keyArr.push(key.toUpperCase().codePointAt(i) - 65);
+		}
+		for (const char of msg.toUpperCase()) {
+			if (/[A-Z]/.test(char)) {
+				result += shiftChar(char);
+			} else {
+				result += char;
+			}
+		}
+		if (!this.#direct) {
+			result = Array.from(result).reverse().join("");
+		}
+		return result;
+
+		function shiftChar(char) {
+			let result = char.codePointAt(0);
+			result = result - getCurrentKey();
+			if (result < 65) {
+				result = result + 26;
+			}
+			return String.fromCharCode(result);
+		}
+
+		function getCurrentKey() {
+			++keyIndex;
+			if (keyIndex >= key.length) {
+				keyIndex = 0;
+			}
+			return keyArr[keyIndex];
+		}
+	}
 }
 
 module.exports = {
-  directMachine: new VigenereCipheringMachine(),
-  reverseMachine: new VigenereCipheringMachine(false),
-  VigenereCipheringMachine,
+	directMachine: new VigenereCipheringMachine(),
+	reverseMachine: new VigenereCipheringMachine(false),
+	VigenereCipheringMachine,
 };
